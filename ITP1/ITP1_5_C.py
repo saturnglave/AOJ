b5ea857e969ca862020324cf12953f1381469cb5
saturnglave <tomo.oyama025@gmail.com>
2018-12-08 18:01:28 +0900

    finish ITP1_5_C

--- ITP1/ITP1_5_C.py
+++ ITP1/ITP1_5_C.py
@@ -3,9 +3,14 @@ while True:
     h, w = map(int, input().split())
     if h == 0 and w == 0:
         break
-    for height in range(0, h+1):
-        if height % 2 == 0:
-            print()
-        else:
-            print()
+    # H+Wが偶数の時は#, 奇数の場合は.が出る
+    for height in range(h):
+        for width in range(w):
+            if (height+width) % 2 == 0:
+                print('#', end = '')
+            else:
+                print('.', end = '')
+        # 次の行へ
+        print ('')
+    # 次のデータセットへ
     print('')
