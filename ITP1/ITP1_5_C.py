3ca3ae4a3331b91b31664f20133d042528e67e3e
saturnglave <tomo.oyama025@gmail.com>
2018-12-08 17:43:47 +0900

    make ITP1_5_C

---
+++ ITP1/ITP1_5_C.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+while True:
+    h, w = map(int, input().split())
+    if h == 0 and w == 0:
+        break
+    for height in range(0, h+1):
+        if height % 2 == 0:
+            print()
+        else:
+            print()
+    print('')
