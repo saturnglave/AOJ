227e974b42a00604ea35cc3b53c2ef47519418f7
saturnglave <tomo.oyama025@gmail.com>
2018-12-17 14:16:35 +0900

    Create ITP1_7_C.py

---
+++ ITP1/ITP1_7_C.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+r, c = map(int, input().split())
+table = [[]*(c+1) for i in range(r+1)]
+
+for i in range(r+1):
+    for j in range(c+1):
+        if j == c+1:
+            table[j] = 0
+        else:
+            table[j] = list(map(int, input().split())
+
+# 計算
+for i in ra
