417ec739db553e3086b1dded58646fb6833fadc2
saturnglave <tomo.oyama025@gmail.com>
2019-01-24 17:45:00 +0900

    Update ITP1_7_C.py

--- ITP1/ITP1_7_C.py
+++ ITP1/ITP1_7_C.py
@@ -1,13 +1,26 @@
 # coding:utf-8
 r, c = map(int, input().split())
-table = [[]*(c+1) for i in range(r+1)]
+table_input = [[0]*(c) for i in range(r)]
+table_ans = [[0]*(c+1) for i in range(r+1)]
+print(table_input)
 
+# table settings
+for i in range(r):
+    table_input[i] = list(map(int, input().split()))
+
+print(table_input)
+
+
+# 計算とテーブル穴埋め
 for i in range(r+1):
     for j in range(c+1):
-        if j == c+1:
-            table[j] = 0
-        else:
-            table[j] = list(map(int, input().split())
+        if i < r and j < c:
+            table_ans[i][j] = table_input[i][j]
+        elif i < r and j == c:
+            table_ans[i][c] = sum(table_input[i])
+        elif i == r and j < c:
+            table_ans[r][j] = 0
 
-# 計算
-for i in ra
+
+
+print(' '.join(map(str, table_ans)))
