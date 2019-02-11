6a1cb3d47ba02687b0cb8a090a0b7918c6055831
saturnglave <tomo.oyama025@gmail.com>
2019-02-11 22:39:34 +0900

    ITP1_8_C

---
+++ ITP1/ITP1_8_C.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+import sys
+import string
+
+text = []
+for l in sys.stdin:
+    text.append(l)
+
+sentence = ''.join(text).lower()
+
+for ltr in string.ascii_lowercase:
+    print('{}:{}'.format(ltr, sentence.count(ltr)))
