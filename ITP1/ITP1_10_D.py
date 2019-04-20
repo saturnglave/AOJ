61cdcc02a22ee8b79f1b02114a45b82c7612e790
saturnglave <tomo.oyama025@gmail.com>
2019-04-20 22:56:21 +0900

    ITP1_11入った

--- ITP1/ITP1_10_D.py
+++ ITP1/ITP1_10_D.py
@@ -2,14 +2,15 @@
 import math
 
 n = int(input())
-x = map(int, input().split())
-y = map(int, input().split())
+x = list(map(int, input().split()))
+y = list(map(int, input().split()))
 
-p1 = sum([(abs(x[i]-y[i]) for i in range(n)])
-p2 = sqrt(sum([pow(abs(x[i]-y[i]), 2) for i in range(n)]))
-p3 = pow(sum([pow(abs(x[i]-y[i]), 3)]),1/3)
+p1 = sum([abs(x[i]-y[i]) for i in range(n)])
+p2 = math.sqrt(sum([pow(abs(x[i]-y[i]), 2) for i in range(n)]))
+p3 = pow(sum([pow(abs(x[i]-y[i]), 3) for i in range(n)]),1/3)
 p = max([abs(x[i] - y[i]) for i in range(n)])
 
 print(p1)
 print(p2)
+print(p3)
 print(p)
---
+++ ITP1/ITP1_11_A.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+class Dice:
+    pass
+
+
+
+
+
+def main():
+    number = map(int, input().split())
+    order = str(input())
+    print(number)
+    print(order)
+    dice = Dice()
+
+if __name__ == "__main__":
+    main()
