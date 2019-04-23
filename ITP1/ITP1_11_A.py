26880d771a6dc4f528db388c812c59b4b1b98197
saturnglave <tomo.oyama025@gmail.com>
2019-04-23 21:26:46 +0900

    ITP1_11_A

--- ITP1/ITP1_11_A.py
+++ ITP1/ITP1_11_A.py
@@ -1,17 +1,34 @@
 # coding: utf-8
 class Dice:
-    pass
+    # 初期化
+    def __init__(self, num):
+        self.d = num
 
+    # 転がした時の動作
+    def rotate(self, direction):
+        if direction == "N":
+            self.d[0], self.d[1], self.d[5], self.d[4] = self.d[1], self.d[5], self.d[4], self.d[0]
 
+        elif direction == "S":
+            self.d[0], self.d[1], self.d[5], self.d[4] = self.d[4], self.d[0], self.d[1], self.d[5]
+
+        elif direction == "E":
+            self.d[0], self.d[2], self.d[5], self.d[3] = self.d[3], self.d[0], self.d[2], self.d[5]
+        else:
+            self.d[0], self.d[2], self.d[5], self.d[3] = self.d[2], self.d[5], self.d[3], self.d[0]
 
 
 
 def main():
-    number = map(int, input().split())
-    order = str(input())
-    print(number)
-    print(order)
-    dice = Dice()
+    num = [int(i) for i in input().split()]
+    direction = str(input())
+    #print(num)
+    #print(direction)
+    dice = Dice(num)
+
+    for i in direction:
+        dice.rotate(i)
+    print(dice.d[0])
 
 if __name__ == "__main__":
     main()
