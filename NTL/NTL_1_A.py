21f656a99e13835d71d0d345e3a8c83097022112
saturnglave <tomo.oyama025@gmail.com>
2019-05-12 22:03:45 +0900

    NTL1

--- NTL/NTL_1_A.py
+++ NTL/NTL_1_A.py
@@ -1,1 +1,24 @@
+#coding:utf-8
+import math
+
+n = int(input())
+m = n
+result=[]
+
+def prime_factor(num, result):
+    i = 2
+    while(i <= math.sqrt(num)):
+        if num % i == 0:
+            num //= i
+            result.append(str(i))
+        else:
+            i += 1
+    
+    if num != 1:
+        result.append(str(num))
+
+if __name__ == "__main__":
+    prime_factor(n, result)
+    print(str(m) + ': ' + ' '.join(result))
+
 
